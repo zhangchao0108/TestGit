5ee7a290c8a2c65ff0cc5c12db6a1858b355c654
zhangchao_global <zhangchao_sc_global@126.com>
2019-11-06 22:31:28 +0800

    commit good.java

---
+++ src/main/java/com/zhangchao/git/good.java
@@ -0,0 +1,14 @@
+package com.zhangchao.git;
+
+public class good {
+
+	public good() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
